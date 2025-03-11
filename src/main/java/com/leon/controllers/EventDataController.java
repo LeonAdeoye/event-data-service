67b3c942e5beb28747fd00fa77b6be03b475eded
LeonAdeoye <leon.adeoye@gmail.com>
2025-03-11 22:16:47 +0800

    Added controller

---
+++ src/main/java/com/leon/controllers/EventDataController.java
@@ -0,0 +1,10 @@
+package com.leon.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/event-data")
+public class EventDataController {
+
+}
